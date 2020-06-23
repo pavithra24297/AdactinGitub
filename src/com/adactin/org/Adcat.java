4c8f98b9dc584bbf7a0010cf2a22578cddfea47b
pavithra24297 <rajalakshmi2497@gmail.com>
2020-06-23 16:11:53 +0530

    first Commit

---
+++ src/com/adactin/org/Adcat.java
@@ -0,0 +1,23 @@
+package com.adactin.org;
+
+public class Adcat {
+	public static void star() {
+
+		for (int i = 0; i < 10; i++) {
+
+			for (int j = 0; j <= i; j++) {
+
+				System.out.print("*");
+
+			}
+			System.out.println();
+		}
+	}
+
+	public static void main(String[] args) {
+		star();
+
+	}
+}
+
+
